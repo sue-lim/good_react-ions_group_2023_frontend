10e10d24f06abc14b44be3e60b225704dfc6c670
KayeDante89 <k.flamenco@hotmail.com>
2023-03-19 14:58:20 +0800

    basic skeleton of Nav

---
+++ skillsworkshop/src/Components/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav>
+      <div id="logo">
+        <img src="https://i.postimg.cc/T14q7SSk/Brown-and-White-Named-Warner-Spencer-Logo-500-350px.png" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Mentors</li>
+        <li>Sign Up</li>
+        <li>Log In</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
