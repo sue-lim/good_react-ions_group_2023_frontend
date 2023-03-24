53628a8ac899d959eefbac8563560966839a1684
KayeDante89 <k.flamenco@hotmail.com>
2023-03-25 06:56:26 +0800

    footer css

--- skillsworkshop/src/Components/Footer/Footer.jsx
+++ skillsworkshop/src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import "./Footer.css";
 
 function Footer() {
-  return <footer id="footer">The Good React-ions 2023</footer>;
+  return <footer id="footer">&copy; The Good React-ions 2023</footer>;
 }
 
 export default Footer;
