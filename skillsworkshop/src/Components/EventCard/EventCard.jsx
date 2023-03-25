fc796263d326a57b9dc00a2b99bf36549015a0ed
KayeDante89 <k.flamenco@hotmail.com>
2023-03-25 12:28:40 +0800

    added images to public

--- skillsworkshop/src/Components/EventCard/EventCard.jsx
+++ skillsworkshop/src/Components/EventCard/EventCard.jsx
@@ -4,15 +4,23 @@ import "./ProjectCard.css";
 import ProgressBar from "../ProgressBar/ProgressBar";
 
 function ProjectCard(props) {
-  const { eventData } = props;
+  const { projectData } = props;
 
+  //   return (
+  //     <div className="project-card">
+  //       <Link className="proj-name-link" to={`/project/${eventData.id}`}>
+  //         <img src={eventData.image} />
+  //         <h2>{eventData.title}</h2>
+  //         <p>{eventData.description}</p>
+  //         <p>created: {new Date(eventData.date_created).toLocaleDateString()}</p>
+  //       </Link>
+  //     </div>
+  //   );
   return (
-    <div className="project-card">
-      <Link className="proj-name-link" to={`/project/${eventData.id}`}>
-        <img src={eventData.image} />
-        <h2>{eventData.title}</h2>
-        <p>{eventData.description}</p>
-        <p>created: {new Date(eventData.date_created).toLocaleDateString()}</p>
+    <div>
+      <Link to="/project">
+        <img src={projectData.image} />
+        <h3>{projectData.title}</h3>
       </Link>
     </div>
   );
--- skillsworkshop/src/Components/HeroSection.jsx
+++ skillsworkshop/src/Components/HeroSection.jsx
@@ -2,7 +2,36 @@ import React from "react";
 import "./HeroSection.css";
 import video from "./videos/office-80314.mp4";
 
+import { allProjects } from "../data";
+
 function HeroSection() {
+  //   const [projectList, setProjectList] = useState([]);
+  //   // const [user, setUser] = useState([]);
+
+  //   // const { id } = useParams();
+
+  //   useEffect(() => {
+  //     fetch(`${import.meta.env.VITE_API_URL}projects`) // making network request to url
+  //       .then((results) => {
+  //         return results.json();
+  //       })
+  //       .then((data) => {
+  //         setProjectList(data);
+  //       });
+  //   }, []);
+
+  //   function compare(a, b) {
+  //     if (a.date_created < b.date_created) {
+  //       return 1;
+  //     }
+  //     if (a.date_created > b.date_created) {
+  //       return -1;
+  //     }
+  //     return 0;
+  //   }
+
+  //   const latestProject = projectList.sort(compare).slice(0, 3);
+
   return (
     <div>
       <div className="main-container">
@@ -25,6 +54,13 @@ function HeroSection() {
       </div>
       <div className="containerevents">
         <h1 className="title">Upcoming events</h1>
+        <div className="project-container">
+          <div id="project-list">
+            {allProjects.map((projectData, key) => {
+              return <div key={key}>{projectData.title}</div>;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
