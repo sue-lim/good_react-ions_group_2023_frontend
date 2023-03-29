37e447392877ee74e9e1110b9b8b73669a81e176
KayeDante89 <k.flamenco@hotmail.com>
2023-03-30 00:01:17 +0800

    edit media query for burger nav

--- skillsworkshop/src/Components/Nav/Nav.jsx
+++ skillsworkshop/src/Components/Nav/Nav.jsx
@@ -1,53 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import "./Nav.css";
-
-// function Nav() {
-//   const myFunction = () => {
-//     var x = document.getElementById("nav-list");
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-//   };
-
-//   return (
-//     <nav className="topnav" id="nav-bar">
-// <div>
-//   <img
-//     id="logo"
-//     src="https://i.postimg.cc/L685TDz1/Brown-and-White-Named-Warner-Spencer-Logo-500-350px-500-150px-1.png"
-//   />
-// </div>
-//       <div id="nav-list">
-//         <Link className="active" to="/">
-//           HOME
-//         </Link>
-//         <Link className="nav-link" to="/#AboutUs">
-//           ABOUT
-//         </Link>
-//         <Link className="nav-link" to="/create-event">
-//           CREATE
-//         </Link>
-//         <Link className="nav-link" to="/sign-up">
-//           SIGN UP
-//         </Link>
-//         <Link className="nav-link" to="/login">
-//           LOGIN
-//         </Link>
-//       </div>
-//       <a href="javascript:void(0);" className="icon" onClick={myFunction}>
-// <span className="bar"></span>
-// <span className="bar"></span>
-// <span className="bar"></span>
-//       </a>
-//     </nav>
-//   );
-// }
-
-// export default Nav;
-
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
@@ -72,9 +22,22 @@ function Nav() {
       </div>
       <div className="topnav" id="myTopnav">
         <a href="#" className="active"></a>
-        <a href="/">NEWS</a>
-        <a href="#contact">CONTACT</a>
-        <a href="#about">ABOUT</a>
+        <Link className="nav-link" to="/login">
+          LOGIN
+        </Link>
+        <Link className="nav-link" to="/sign-up">
+          SIGN UP
+        </Link>
+        <Link className="nav-link" to="/create-event">
+          CREATE
+        </Link>
+        <Link className="nav-link" to="/#AboutUs">
+          ABOUT
+        </Link>
+        <Link className="nav-link" to="/">
+          HOME
+        </Link>
+
         <a href="javascript:void(0);" className="icon" onClick={myFunction}>
           <span className="bar"></span>
           <span className="bar"></span>
