a27ebad0000c7a8ca06e902d67076fa7c8354b70
jackybalfour <jacky.balfour@gmail.com>
2023-04-01 10:12:47 +0800

    userpage update

--- skillsworkshop/src/Components/UserProfile/UserProfile.jsx
+++ skillsworkshop/src/Components/UserProfile/UserProfile.jsx
@@ -1,12 +1,58 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./UserProfile.css";
 
 function UserProfile() {
+    const [userData, setUserData] = useState([]);
+
+    const { id } = useParams()
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}users/${id}/`)
+
+            .then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setUserData(data);
+            });
+    }, []);
+
     return (
         <div>
 
-            <h1 className="username"> Nina Smith</h1>
-            <h2 className="email">email@email.com</h2>
-            <h2 className="location">Perth, Australia</h2>
+            <img className="project-card-img" src={userData.profile_picture} />
+            <h3 className="project-card-name">{userData.username}</h3>
+
+            <div className="profile">
+                <div className="containerprofile">
+                    <img className="userphoto" src={userData.profile_picture} alt="User Photo"></img>
+                    <div className="userdetails">
+                        <h1 className="title">{userData.first_name} {userData.last_name}</h1>
+                        <div className="containerdetails">
+                            <img className="icon" src="skillsworkshop\public\email.png"></img><p className="details">{userData.email}</p>
+                        </div>
+                        <div className="containerdetails">
+                            <img className="icon" src="public/location.png"></img><p className="details">{userData.location}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="bioandskills">
+
+                    <div className="skillscontainer">
+                        <h1 className="title"> Skills:</h1>
+                        <li> REACT</li>
+                    </div>
+
+                    <div className="biocontainer">
+                        <h1 className="title"> BIO:</h1>
+                        <p>{userData.bio}</p>
+                    </div>
+
+                </div>
+            </div>
+
 
 
         </div>
@@ -14,4 +60,5 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
 
