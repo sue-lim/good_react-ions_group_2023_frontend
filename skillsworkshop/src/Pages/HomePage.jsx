7d057b02829a58bc3bb4736962c8b526ee161243
KayeDante89 <k.flamenco@hotmail.com>
2023-04-01 20:06:43 +0800

    Update HomePage.jsx
    removed code

--- skillsworkshop/src/Pages/HomePage.jsx
+++ skillsworkshop/src/Pages/HomePage.jsx
@@ -2,38 +2,7 @@ import "../App.css";
 import HeroSection from "../Components/HeroSection";
 
 function Home() {
-  const [workshopList, setWorkshopList] = useState([]);
-
-  const { id } = useParams;
-
-  const authToken = window.localStorage.getItem("token");
-
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}events`) // making network request to url
-      .then((results) => {
-        return results.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setWorkshopList(data);
-      });
-  }, []);
-
-  // useEffect(() => {
-  //   fetch(`${import.meta.env.VITE_API_URL}users/1/`) // making network request to url
-  //     .then((results) => {
-  //       return results.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setWorkshopList(data);
-  //     });
-  // }, []);
-
-  return (
-    // <div>
-
-    // </div>
-    <HeroSection />
-  );
+  return <HeroSection />;
 }
+
+export default Home;
