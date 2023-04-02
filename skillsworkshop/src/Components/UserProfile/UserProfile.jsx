6fbdf2d1442023be4f0b7c68ddfe1c01e4fbda53
jackybalfour <jacky.balfour@gmail.com>
2023-04-02 21:11:29 +0800

    userprofile

--- skillsworkshop/src/Components/UserProfile/UserProfile.jsx
+++ skillsworkshop/src/Components/UserProfile/UserProfile.jsx
@@ -3,11 +3,9 @@ import { useParams, Link } from "react-router-dom";
 import "./UserProfile.css";
 
 function UserProfile() {
-  const [userData, setUserData] = useState([]);
+  const [UserData, setUserData] = useState([]);
 
   const { id } = useParams();
-
-  //   const getToken = localStorage.getItem("token");
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}users/${id}/`)
@@ -15,54 +13,48 @@ function UserProfile() {
         return results.json();
       })
       .then((data) => {
-        // console.log(data);
         setUserData(data);
       });
   }, []);
 
   return (
-    <div className="userpage">
-      <div className="profile">
-        <div className="containerprofile">
-          <img
-            className="userphoto"
-            src={userData.profile_picture}
-            alt="User Photo"
-          />
 
-          <div className="userdetails">
-            <h1 className="title">{userData.first_name}</h1>
-            <div className="containerdetails">
-              <img
-                className="icon"
-                src="https://i.postimg.cc/W1VnDdts/email.png"
-              ></img>
-              <p className="details">{userData.email}</p>
-            </div>
-            <div className="containerdetails">
-              <img
-                className="icon"
-                src="https://i.postimg.cc/JzRNrSdn/location.png"
-              ></img>
-              <p className="details">{userData.location}</p>
-            </div>
+    <div className="profile">
+
+      <div className="containerprofile">
+        <img className="userphoto" src={UserData.profile_picture} alt="User Photo"></img>
+        <div className="userdetails">
+          <h1 className="user_name">{UserData.first_name} {UserData.last_name}</h1>
+          <div className="containerdetails">
+            <img className="icon" src="https://i.postimg.cc/W1VnDdts/email.png"></img>
+            <p className="details">{UserData.email}</p>
           </div>
-        </div>
-
-        <div className="bioandskills">
-          <div className="skillscontainer">
-            <h1 className="title"> Skills:</h1>
-            <li> REACT</li>
-          </div>
-
-          <div className="biocontainer">
-            <h1 className="title"> BIO:</h1>
-            <p>{userData.bio}</p>
+          <div className="containerdetails">
+            <img className="icon" src="https://i.postimg.cc/JzRNrSdn/location.png"></img><p className="details">{UserData.location}</p>
           </div>
         </div>
       </div>
+
+      <div className="bioandskills">
+
+        <div className="skillscontainer">
+          <h1 className="user_titles"> Skills:</h1>
+          <p>{UserData.skills}</p>
+        </div>
+
+        <div className="biocontainer">
+          <h1 className="user_titles"> BIO:</h1>
+          <p>{UserData.bio}</p>
+        </div>
+
+      </div>
+
+
+
+
     </div>
   );
 }
 
 export default UserProfile;
+
