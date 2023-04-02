75d3ec856fe1bc5f4bf88060e63b3d5135052da0
KayeDante89 <k.flamenco@hotmail.com>
2023-04-02 20:51:35 +0800

    Deleted un-used code

--- skillsworkshop/src/Components/HeroSection.jsx
+++ skillsworkshop/src/Components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import "./HeroSection.css";
 import video from "./videos/office-80314.mp4";
 
