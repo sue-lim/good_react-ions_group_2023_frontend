04092b6962a7907b33a2ea0c0a9cbe126446b8a3
KayeDante89 <k.flamenco@hotmail.com>
2023-04-10 18:47:17 +0800

    tweaked MQ for user profile

--- skillsworkshop/src/Components/UserProfile/UserProfile.jsx
+++ skillsworkshop/src/Components/UserProfile/UserProfile.jsx
@@ -18,25 +18,35 @@ function UserProfile() {
   }, []);
 
   return (
-
     <div className="profile">
-
+      {" "}
       <div className="containerprofile">
-        <img className="userphoto" src={UserData.profile_picture} alt="User Photo"></img>
+        <img
+          className="userphoto"
+          src={UserData.profile_picture}
+          alt="User Photo"
+        ></img>
         <div className="userdetails">
-          <h1 className="user_name">{UserData.first_name} {UserData.last_name}</h1>
+          <h1 className="user_name">
+            {UserData.first_name} {UserData.last_name}
+          </h1>
           <div className="containerdetails">
-            <img className="icon" src="https://i.postimg.cc/W1VnDdts/email.png"></img>
+            <img
+              className="icon"
+              src="https://i.postimg.cc/W1VnDdts/email.png"
+            ></img>
             <p className="details">{UserData.email}</p>
           </div>
           <div className="containerdetails">
-            <img className="icon" src="https://i.postimg.cc/JzRNrSdn/location.png"></img><p className="details">{UserData.location}</p>
+            <img
+              className="icon"
+              src="https://i.postimg.cc/JzRNrSdn/location.png"
+            ></img>
+            <p className="details">{UserData.location}</p>
           </div>
         </div>
       </div>
-
       <div className="bioandskills">
-
         <div className="skillscontainer">
           <h1 className="user_titles"> Skills:</h1>
           <ul>
@@ -48,15 +58,9 @@ function UserProfile() {
           <h1 className="user_titles"> BIO:</h1>
           <p>{UserData.bio}</p>
         </div>
-
       </div>
-
-
-
-
     </div>
   );
 }
 
 export default UserProfile;
-
